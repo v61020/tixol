70806691bb81283b5ce290f5c7a7fb0b5c1abab3
vcxiz <32043469+vcxiz@users.noreply.github.com>
2017-10-15 16:10:54 -0500

    Update Teh-Bot.js

--- Teh-Bot.js
+++ Teh-Bot.js
@@ -1,21 +1,25 @@
-
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
-const prefix = config.prefix;
- 
+const config = require("./config.json")
+const prefix = config.prefix
 
 client.on("ready", () => {
-  console.log("I am ready!")
-};
+  console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-  // Exit and stop if it's not there
-  if (!message.content.startsWith(config.prefix)) return;
-   const args = message.content.slice(prefix.length).trim().split(/ +/g);
+ const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  switch (command) {
+ switch (command) {
   case "ping" :
     message.channel.send('Pong!');
     break;
@@ -30,6 +34,8 @@ client.on("message", (message) => {
     message.reply(rand + ".")
    break;
   }
-  });
+});
+
 
 client.login(process.env.BOT_TOKEN);
+
