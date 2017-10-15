22adb44c59e78b1ad73fcb4e8da046041055209e
vcxiz <32043469+vcxiz@users.noreply.github.com>
2017-10-15 06:37:53 -0500

    Update Teh-Bot.js

--- Teh-Bot.js
+++ Teh-Bot.js
@@ -1,13 +1,16 @@
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const prefix = "t!"
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
+   // Exit and stop if it's not there
+  if (!message.content.startsWith(prefix)) return;
+  if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
   }
 });
