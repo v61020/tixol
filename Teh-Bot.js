f3b9b3b2ad26c0b99d9ec7e8744fbc6f88698428
vcxiz <32043469+vcxiz@users.noreply.github.com>
2017-10-15 16:01:13 -0500

    Update Teh-Bot.js

--- Teh-Bot.js
+++ Teh-Bot.js
@@ -24,6 +24,12 @@ client.on("message", (message) => {
     message.channel.send("Sending a meow!")
     member.send("Meow!").catch( message.channel.send("Completed!") )
     break;
-}});
+   case "8ball" :
+    var myArray = ["It is certain", "As I see it, yes", "It is decidedly so", "Without a doubt" "Yes definitely", "You may rely on it", "Outlook good", "Most likely", "Yes", "Signs point to yes", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again"];
+    var rand = myArray[Math.floor(Math.random() * myArray.length)];
+    message.reply(rand + ".")
+    break;
+  }
+  });
 
 client.login(process.env.BOT_TOKEN);
