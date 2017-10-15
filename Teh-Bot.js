2eb687f238e0e12130ebff5d1d568c44bd17108c
vcxiz <32043469+vcxiz@users.noreply.github.com>
2017-10-15 06:42:42 -0500

    Update Teh-Bot.js

--- Teh-Bot.js
+++ Teh-Bot.js
@@ -1,18 +1,24 @@
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "t!"
+const config = require("./config.json");
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-   // Exit and stop if it's not there
-  if (!message.content.startsWith(prefix)) return;
-  if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("pong!");
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(config.prefix)) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if (command === "ping")) {
+    message.channel.send("Pong!");
+  }
+  if (command === "meow")) {
+    let member = message.mentions.members.first();
+    message.channel.send(member + ", Meow!");
   }
 });
 
-client.login("SuperSecretBotTokenHere");
+client.login(config.token);
