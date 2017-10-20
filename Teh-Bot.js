87438e38c24167beefc4c5f3584b7f8084f0570c
vcxiz <32043469+vcxiz@users.noreply.github.com>
2017-10-20 15:20:29 -0500

    Update Teh-Bot.js

--- Teh-Bot.js
+++ Teh-Bot.js
@@ -19,6 +19,34 @@ client.on("message", (message) => {
   case "foo" :
     message.channel.send('Bar!');
     break;
+   case "8ball" :
+         	var sayings = ["It is certain",
+										"It is decidedly so",
+										"Without a doubt",
+										"Yes, definitely",
+										"You may rely on it",
+										"As I see it, yes",
+										"Most likely",
+										"Outlook good",
+										"Yes",
+										"Signs point to yes",
+										"Reply hazy try again",
+										"Ask again later",
+										"Better not tell you now",
+										"Cannot predict now",
+										"Concentrate and ask again",
+										"Don't count on it",
+										"My reply is no",
+										"My sources say no",
+										"Outlook not so good",
+										"Very doubtful"];
+
+		 var result = Math.floor((Math.random() * sayings.length) + 0);
+     bot.reply(message, sayings[result]);
+     break;
+  case "help" :
+    message.channel.send('Sending help');
+    break;
 }
 });
 
