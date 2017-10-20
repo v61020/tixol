cdee59719df9e04c60306ec3911d3e22db9400a0
vcxiz <32043469+vcxiz@users.noreply.github.com>
2017-10-20 15:16:14 -0500

    Update Teh-Bot.js

--- Teh-Bot.js
+++ Teh-Bot.js
@@ -1,36 +1,25 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json")
-const prefix = config.prefix
 
-client.on("ready", () => {
-  console.log("I am ready!");
-});
-
+// Set the prefix
+let prefix = "t!";
 client.on("message", (message) => {
- const Discord = require("discord.js");
-const client = new Discord.Client();
-
-client.on("ready", () => {
-  console.log("I am ready!");
-});
-
-client.on("message", (message) => {
+  // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+
  switch (command) {
   case "ping" :
     message.channel.send('Pong!');
     break;
-  case "meow" :
-    let member = message.mentions.members.first();
-    message.channel.send("Sending a meow!")
-    member.send("Meow!").catch( message.channel.send("Completed!"))
+  case "blah" :
+    message.channel.send('Meh.');
     break;
-  }
+  case "foo" :
+    message.channel.send('Bar!');
+    break;
+}
 });
 
-
 client.login(process.env.BOT_TOKEN);
-
